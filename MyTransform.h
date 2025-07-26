a2da59057a8c575a08e0dd4afc15794e2cdfee2e
hoankhtn <hoanbk006@gmail.com>
2025-07-26 09:47:09 +0700

    Upload file MyTransform (.h and .cpp)

---
+++ MyTransform.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Point2D.h"
+#include <iostream>
+using namespace std;
+
+class MyTransform {
+private:
+	float translateX = 0.0;
+	float translateY = 0.0;
+	float scaleX = 1.0;
+	float scaleY = 1.0;
+	float rotateD = 0.0;
+public:
+	MyTransform();
+
+	//setter
+	void setTranslate(float, float);
+	void setScale(float, float);
+	void setUniformScale(float); // Uniform scale
+	void setRotate(float);
+	//getter
+	Point2D getTranslate() const;
+	Point2D getScale() const;
+	float getRotate() const;
+};
