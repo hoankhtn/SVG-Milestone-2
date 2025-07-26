52c169c0c4bf2e94df645687924cd0b4eaf89181
TienThanh1234 <tienthanhphr@gmail.com>
2025-07-26 21:46:01 +0700

    QuadraticBezierCommand.cpp

--- QuadraticBezierCommand.cpp
+++ QuadraticBezierCommand.cpp
@@ -32,5 +32,47 @@ void QuadraticBezierCommand::print(std::ostream& os) const {
 }
 
 void QuadraticBezierCommand::execute(Graphics& g, Pen* pen, Point2D& currentPoint, Point2D& lastControlPoint, char& previousCmd) {
+    // Draw quadratic Bezier curve
+    float currX = currentPoint.getPointX();
+    float currY = currentPoint.getPointY();
 
+    float ctrlX, ctrlY, endX, endY;
+
+    if (relative) {
+        ctrlX = currX + getX1();
+        ctrlY = currY + getY1();
+        endX = currX + getX();
+        endY = currY + getY();
+        previousCmd = 'q';
+    }
+    else {
+        ctrlX = getX1();
+        ctrlY = getY1();
+        endX = getX();
+        endY = getY();
+        previousCmd = 'Q';
+    }
+
+    PointF P0(currX, currY);
+    PointF P1(ctrlX, ctrlY);
+    PointF P2(endX, endY);
+
+
+    PointF C0 = P0;
+    PointF C1(
+        P0.X + 2.0f / 3.0f * (P1.X - P0.X),
+        P0.Y + 2.0f / 3.0f * (P1.Y - P0.Y)
+    );
+    PointF C2(
+        P2.X + 2.0f / 3.0f * (P1.X - P2.X),
+        P2.Y + 2.0f / 3.0f * (P1.Y - P2.Y)
+    );
+    PointF C3 = P2;
+
+    if (pen) {
+        g.DrawBezier(pen, C0, C1, C2, C3);
+    }
+
+    currentPoint = Point2D(endX, endY);
+    lastControlPoint = Point2D(ctrlX, ctrlY);
 }
