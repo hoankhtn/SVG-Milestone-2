8ecd829280922812dd3f931b50cea1a83e4b0cfe
hoankhtn <hoanbk006@gmail.com>
2025-07-29 18:33:20 +0700

    Rename LoadSVG -> LoadFileSVG

--- SVG.cpp
+++ SVG.cpp
@@ -2,7 +2,7 @@
 #include "BackgroundDecorator.h"
 #include "BorderDecorator.h"
 #include "TextDecorator.h"
-#include "LoadSVG.h"
+#include "LoadFileSVG.h"
 
 vector<Shape*> shapes;
 
@@ -72,104 +72,104 @@ void LoadFileSVG::LoadSVG(const string& fileName) {
     }
 }
 
-int main() {
-    LoadFileSVG loader;
-    loader.LoadSVG("svg-04.svg");
+//int main() {
+//    LoadFileSVG loader;
+//    loader.LoadSVG("svg-04.svg");
+//    for (Shape* shape : shapes)
+//        delete shape;
+//    shapes.clear();
+//    return 0;
+//}
+
+VOID OnPaint(HDC hdc)
+{
+   Graphics graphics(hdc);
+
     for (Shape* shape : shapes)
-        delete shape;
-    shapes.clear();
-    return 0;
+    {
+        if (shape)
+            shape->draw(graphics);
+    }
 }
 
-//VOID OnPaint(HDC hdc)
-//{
-//   Graphics graphics(hdc);
-//
-//    for (Shape* shape : shapes)
-//    {
-//        if (shape)
-//            shape->draw(graphics);
-//    }
-//}
-//
-//LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-//
-//INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR lpCmdLine, INT iCmdShow)
-//{
-//    HWND                hWnd;
-//    MSG                 msg;
-//    WNDCLASS            wndClass;
-//    GdiplusStartupInput gdiplusStartupInput;
-//    ULONG_PTR           gdiplusToken;
-//
-//    // Initialize GDI+.
-//    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-//
-//    string fileName = lpCmdLine;
-//    LoadFileSVG loader;
-//    loader.LoadSVG(fileName);
-//
-//    wndClass.style = CS_HREDRAW | CS_VREDRAW;
-//    wndClass.lpfnWndProc = WndProc;
-//    wndClass.cbClsExtra = 0;
-//    wndClass.cbWndExtra = 0;
-//    wndClass.hInstance = hInstance;
-//    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-//    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-//    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-//    wndClass.lpszMenuName = NULL;
-//    wndClass.lpszClassName = TEXT("GettingStarted");
-//
-//    RegisterClass(&wndClass);
-//
-//    hWnd = CreateWindow(
-//        TEXT("GettingStarted"),   // window class name
-//        TEXT("SVG"),  // window caption
-//        WS_OVERLAPPEDWINDOW,      // window style
-//        CW_USEDEFAULT,            // initial x position
-//        CW_USEDEFAULT,            // initial y position
-//        CW_USEDEFAULT,            // initial x size
-//        CW_USEDEFAULT,            // initial y size
-//        NULL,                     // parent window handle
-//        NULL,                     // window menu handle
-//        hInstance,                // program instance handle
-//        NULL);                    // creation parameters
-//
-//    ShowWindow(hWnd, iCmdShow);
-//    UpdateWindow(hWnd);
-//
-//    while (GetMessage(&msg, NULL, 0, 0))
-//    {
-//        TranslateMessage(&msg);
-//        DispatchMessage(&msg);
-//    }
-//
-//    GdiplusShutdown(gdiplusToken);
-//    return msg.wParam;
-//}  // WinMain
-//
-//LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
-//    WPARAM wParam, LPARAM lParam)
-//{
-//    switch (message)
-//    {
-//    case WM_PAINT:
-//    {
-//        PAINTSTRUCT ps;
-//        HDC hdc = BeginPaint(hWnd, &ps);
-//
-//        OnPaint(hdc);
-//
-//        EndPaint(hWnd, &ps);
-//        return 0;
-//    }
-//    case WM_DESTROY:
-//        for (Shape* shape : shapes)
-//            delete shape;
-//        shapes.clear();
-//
-//        PostQuitMessage(0);
-//        return 0;
-//    }
-//    return DefWindowProc(hWnd, message, wParam, lParam);
-//}
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR lpCmdLine, INT iCmdShow)
+{
+    HWND                hWnd;
+    MSG                 msg;
+    WNDCLASS            wndClass;
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR           gdiplusToken;
+
+    // Initialize GDI+.
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+    /*string fileName = lpCmdLine;*/
+    LoadFileSVG loader;
+    loader.LoadSVG("svg-11.svg");
+
+    wndClass.style = CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc = WndProc;
+    wndClass.cbClsExtra = 0;
+    wndClass.cbWndExtra = 0;
+    wndClass.hInstance = hInstance;
+    wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndClass.lpszMenuName = NULL;
+    wndClass.lpszClassName = TEXT("GettingStarted");
+
+    RegisterClass(&wndClass);
+
+    hWnd = CreateWindow(
+        TEXT("GettingStarted"),   // window class name
+        TEXT("SVG"),  // window caption
+        WS_OVERLAPPEDWINDOW,      // window style
+        CW_USEDEFAULT,            // initial x position
+        CW_USEDEFAULT,            // initial y position
+        CW_USEDEFAULT,            // initial x size
+        CW_USEDEFAULT,            // initial y size
+        NULL,                     // parent window handle
+        NULL,                     // window menu handle
+        hInstance,                // program instance handle
+        NULL);                    // creation parameters
+
+    ShowWindow(hWnd, iCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    GdiplusShutdown(gdiplusToken);
+    return msg.wParam;
+}  // WinMain
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+    WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        OnPaint(hdc);
+
+        EndPaint(hWnd, &ps);
+        return 0;
+    }
+    case WM_DESTROY:
+        for (Shape* shape : shapes)
+            delete shape;
+        shapes.clear();
+
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
