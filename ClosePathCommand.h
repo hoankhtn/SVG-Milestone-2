ba628df17470532e7cb2ea4a25b58117f9360e02
TienThanh1234 <tienthanhphr@gmail.com>
2025-07-29 15:53:22 +0700

    ClosePathCommand.h

--- ClosePathCommand.h
+++ ClosePathCommand.h
@@ -10,6 +10,6 @@ public:
 	// Getter declarations
 	Point2D getStartPoint() const;
 	void print(ostream&) const override;//Use for debug
-	void execute(Graphics&, Pen*, Point2D&, Point2D&, char&) override;
+	void execute(Graphics&, Pen*, GraphicsPath*, Point2D&, Point2D&, char&) override;
 };
 
