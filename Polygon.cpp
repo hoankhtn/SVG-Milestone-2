2952e448c7b0cceacb5a3672299ad43d7747aba2
hoankhtn <hoanbk006@gmail.com>
2025-08-03 17:32:52 +0700

    Fix attribute trasform in class polygon

--- Polygon.cpp
+++ Polygon.cpp
@@ -9,20 +9,40 @@ vector<Point2D> MyPolygon::getPoints() const {
     return this->pts;
 }
 
+void MyPolygon::setFill(const Color& fill) {
+    this->fill = fill;
+}
+
 Color MyPolygon::getFill() const {
     return this->fill;
+}
+
+void MyPolygon::setFillOpacity(float fillOpacity) {
+    this->fillOpacity = fillOpacity;
 }
 
 float MyPolygon::getFillOpacity() const {
     return this->fillOpacity;
 }
 
+void MyPolygon::setStroke(const Color& stroke) {
+    this->stroke = stroke;
+}
+
 Color MyPolygon::getStroke() const {
     return this->stroke;
 }
 
+void MyPolygon::setStrokeWidth(float strokeWidth) {
+    this->strokeWidth = strokeWidth;
+}
+
 float MyPolygon::getStrokeWidth() const {
     return this->strokeWidth;
+}
+
+void MyPolygon::setStrokeOpacity(float) {
+    this->strokeOpacity = strokeOpacity;
 }
 
 float MyPolygon::getStrokeOpacity() const {
@@ -30,19 +50,36 @@ float MyPolygon::getStrokeOpacity() const {
 }
 
 void MyPolygon::draw(Graphics& graphics) {
+    if (!transform) return;
     if (pts.size() < 3) return;
 
-    std::vector<Point> transformedPoints;
+    float sx = transform->getScale().getPointX();
+    float sy = transform->getScale().getPointY();
+    float tx = transform->getTranslate().getPointX();
+    float ty = transform->getTranslate().getPointY();
+    float rotate = transform->getRotate();
+
+    Matrix m;
+    m.Scale(sx, sy);
+    m.Rotate(rotate);
+    m.Translate(tx, ty);
+
+    vector<PointF> pointsF;
     for (const auto& pt : pts) {
-        Point2D t = transform->applyToPoint(pt);
-        transformedPoints.emplace_back(Point((INT)t.getPointX(), (INT)t.getPointY()));
+        pointsF.emplace_back(PointF(pt.getPointX(), pt.getPointY()));
+    }
+    m.TransformPoints(pointsF.data(), static_cast<INT>(pointsF.size()));
+
+    if (fillOpacity > 0.0f) {
+        Color fillColor((BYTE)(fillOpacity * 255), fill.GetR(), fill.GetG(), fill.GetB());
+        SolidBrush brush(fillColor);
+        graphics.FillPolygon(&brush, pointsF.data(), static_cast<INT>(pointsF.size()));
     }
 
-    Color fillColor = Color((BYTE)(fillOpacity * 255), fill.GetR(), fill.GetG(), fill.GetB());
-    SolidBrush brush(fillColor);
-    graphics.FillPolygon(&brush, transformedPoints.data(), static_cast<INT>(transformedPoints.size()));
-
-    Color strokeColor = Color((BYTE)(strokeOpacity * 255), stroke.GetR(), stroke.GetG(), stroke.GetB());
-    Pen pen(strokeColor, strokeWidth);
-    graphics.DrawPolygon(&pen, transformedPoints.data(), static_cast<INT>(transformedPoints.size()));
+    if (strokeOpacity > 0.0f && strokeWidth > 0.0f) {
+        Color strokeColor((BYTE)(strokeOpacity * 255), stroke.GetR(), stroke.GetG(), stroke.GetB());
+        Pen pen(strokeColor, strokeWidth);
+        graphics.DrawPolygon(&pen, pointsF.data(), static_cast<INT>(pointsF.size()));
+    }
 }
+
--- Polygon.h
+++ Polygon.h
@@ -13,10 +13,15 @@ private:
 	float strokeOpacity;
 public:
 	MyPolygon(const Color&, float, const vector<Point2D>&, const Color&, float , float);
+	void setFill(const Color&);
 	Color getFill() const;
+	void setFillOpacity(float);
 	float getFillOpacity() const;
+	void setStroke(const Color&);
 	Color getStroke() const;
+	void setStrokeWidth(float);
 	float getStrokeWidth() const;
+	void setStrokeOpacity(float);
 	float getStrokeOpacity() const;
 	vector<Point2D> getPoints() const;
 	void draw(Graphics&) override;
